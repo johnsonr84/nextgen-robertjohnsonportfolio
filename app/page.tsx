627107e4b3e22d2c33dcb41a00b8a41d54d61467
Dev Dork <robertcjohnson1984@gmail.com>
2025-11-28 19:54:14 -0700

    delete file

--- app/page.tsx
+++
@@ -1,9 +0,0 @@
-import PortfolioContent from "../../components/PortfolioContent";
-
-export default async function Home() {
-  return (
-    <main className="min-h-screen">
-      <PortfolioContent />
-    </main>
-  );
-}
